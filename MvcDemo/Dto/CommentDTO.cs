895bfc4d57cac3de71504051833f074242b3a955
fallenangelx <fallen.angel.pp@gmail.com>
2015-05-18 23:05:40 -0300

    attempt to make gitignore work

--- MvcDemo/Dto/CommentDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MvcDemo.Dto
-{
-    public class CommentDTO
-    {
-        public string Comment { get; set; }
-        public int PostId { get; set; }
-        public int UserId { get; set; }
-    }
-}
--- MvcDemo/Dto/PostDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MvcDemo.Dto
-{
-    public class PostDTO
-    {
-        public string Title { get; set; }
-        public string Post { get; set; }
-        public int UserId { get; set; }
-    }
-}
